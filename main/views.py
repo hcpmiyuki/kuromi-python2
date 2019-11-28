f18edce98ccf28f6b068e18446897fee13dfd98d
kimura miyuki <hcp.miyuki@gmail.com>
2019-11-28 12:01:18 +0900

    返信機能追加

--- main/views.py
+++ main/views.py
@@ -1,6 +1,9 @@
 import flask 
 from flask import Flask, Response, request, jsonify
 from main import app
+from janome.tokenizer import Tokenizer
+
+t = Tokenizer()
 
 @app.route('/')
 def show_entries():
@@ -12,6 +15,14 @@ def get_msg():
     msg = request.form['msg']
     if msg != "":
         print(msg)
+        base_list = [token.base_form for token in t.tokenize(msg)]
+        if "かわいい" in base_list:
+            print("ありがとう")
+        elif "疲れ" in base_list:
+            print("おつかれさま♪")
+        else:
+            print("うふふ")
+
         return jsonify({'message':msg})
     else:
         return jsonify({'message':'失敗'})
